33f2b3cbe3d6b00ad627f1cfcf7dad26d5ac0a5f
krb686@gmail.com <krb686@gmail.com>
2017-07-05 23:21:06 -0400

    Remove a bunch of warnings

--- src/reverser.c
+++ src/reverser.c
@@ -13,10 +13,20 @@
 #include <unistd.h>
 #define EXIT_BAD_ARGS 1
 
+//TODO - objects to create
+//  - elf header
+//  - segment header table
+//  - segment header table entry
+//  - segment
+//  - section header table
+//  - section header table entry
+//  - section
+
 // Function prototypes
 void usage();
-void dump_ehdr(Elf64_Ehdr *ehdr);
-void dump_phdr(Elf64_Phdr *phdr, int num_entries);
+void dump_ehdr(Elf64_Ehdr*);
+void dump_phdr(Elf64_Phdr*, int );
+void dump_shdr(Elf64_Shdr*, int );
 
 
 // Getopt variables
@@ -33,8 +43,14 @@ const char *EI_DATA_TYPES[3]  = { "", "LSB", "MSB" };
 const char *EI_OSABI_TYPES[16] = { "System V ABI", "HP-UX", "NetBSD", "Linux", "", "", "Solaris", "AIX", "IRIX", "FreeBSD", "", "", "OpenBSD", "OpenVMS", "NonStop Kernel", "AROS" };
 const char *SEGMENT_TYPES[8] = { "PT_NULL", "PT_LOAD", "PT_DYNAMIC", "PT_INTERP", "PT_NOTE", "PT_SHLIB", "PT_PHDR", "" };
 const char *SEGMENT_FLAG_TYPES[8] = { "---", "--X", "-W-", "-WX", "R--", "R-X", "RW-", "RWX" };
+const char *SECTION_TYPES[12] = { "SHT_NULL", "SHT_PROGBITS", "SHT_SYMTAB", "SHT_STRTAB", "SHT_RELA", "SHT_HASH", "SHT_DYNAMIC", "SHT_NOTE", "SHT_NOBITS", "SHT_REL", "SHT_SHLIB", "SHT_DYNSYM" };
+const char *SECTION_FLAG_TYPES[8] = { "", "write", "alloc", "alloc+write", "exec", "exec+write", "exec+alloc", "exec+alloc+write" };
 
 Elf64_Ehdr *ehdr;
+
+struct section {
+    char* name;
+};
 
 // Begin
 int main(int argc, char* argv[]){
@@ -42,6 +58,7 @@ int main(int argc, char* argv[]){
     int c;
     int opt_index;
     char *filename;
+    char *incptr;
     struct stat fstatbuf;
     Elf64_Phdr *phdr_array;
     Elf64_Shdr *shdr_array;
@@ -68,17 +85,20 @@ int main(int argc, char* argv[]){
     off_t filesize;
     filesize = lseek(fd, 0, SEEK_END);
     ehdr = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fd, 0); 
+    printf("ehdr located at %p\n", ehdr);
 
     dump_ehdr(ehdr);
 
 
     // Locate and dump the program header table
-    phdr_array = (char*)ehdr + ehdr->e_phoff;
+    incptr = (char*)ehdr;
+    phdr_array = (Elf64_Phdr*)(incptr + ehdr->e_phoff);
+    printf("phdr_array located at %p\n", phdr_array);
     dump_phdr(phdr_array, ehdr->e_phnum);
 
     // Locate and dump the section header table
-    shdr_array = (char*)ehdr + ehdr->e_shoff;
-    printf("shdr_array = 0x%" PRIx64 "\n", shdr_array);
+    shdr_array = (Elf64_Shdr*)(incptr + ehdr->e_shoff);
+    printf("shdr_array = %p\n", shdr_array);
     dump_shdr(shdr_array, ehdr->e_shnum);
     return 0;
 }
@@ -112,7 +132,7 @@ void dump_phdr(Elf64_Phdr *phdr, int num_entries){
     for(i=0;i<num_entries;i++){
         printf("\tEntry #%d\n", i+1);
 
-        char *segtype;
+        const char *segtype;
         if(phdr->p_type < 7){
             segtype = SEGMENT_TYPES[phdr->p_type];
         } else {
@@ -120,7 +140,7 @@ void dump_phdr(Elf64_Phdr *phdr, int num_entries){
         }
         printf("\t\tp_type = 0x%" PRIx32 " = %s" "\n", phdr->p_type, segtype);
 
-        char *pflagtype;
+        const char *pflagtype;
         if(phdr->p_flags < 8){
             pflagtype = SEGMENT_FLAG_TYPES[phdr->p_flags];
         } else {
@@ -140,25 +160,65 @@ void dump_phdr(Elf64_Phdr *phdr, int num_entries){
 void dump_shdr(Elf64_Shdr *shdr, int num_entries){
     printf("======== SECTION HEADER TABLE ========\n");
 
+    char *incptr;
+
     // Get the string table entry
     // It is the section header table's address + (e_shstrndx * size of an entry)
-    Elf64_Shdr *sh_strentry = ((char*)shdr + (ehdr->e_shstrndx * sizeof(Elf64_Shdr)));
-    printf("\t\tsh_strentry = 0x%" PRIx64 "\n", sh_strentry);
+    incptr = (char*)shdr;
+    Elf64_Shdr *sh_strentry = (Elf64_Shdr*)(incptr + (ehdr->e_shstrndx * sizeof(Elf64_Shdr)));
+    printf("\t\tsh_strentry = %p\n", sh_strentry);
 
     // Get the strtab address
     char *strtab = (char*)ehdr + sh_strentry->sh_offset;
-    printf("\t\tstrtab = 0x%" PRIx64 "\n", strtab);
+    printf("\t\tstrtab located at %p\n", strtab);
 
     int i;
     for(i=0;i<num_entries;i++){
         printf("\tSection #%d\n", i);
-        printf("\t\tshdr located at - 0x%" PRIx64 "\n", shdr);
+        printf("\t\tshdr located at - %p\n", shdr);
         char *str_section = strtab + shdr->sh_name;
         printf("\t\tsh_name(index) = 0x%" PRIu32 " = %s" "\n", shdr->sh_name, str_section );
-        printf("\t\tsh_type = 0x%" PRIx32 "\n", shdr->sh_type);
-        printf("\t\tsh_flags = 0x%" PRIx32 "\n", shdr->sh_flags);
+
+        const char *sh_type_str;
+        int sh_type = shdr->sh_type;
+        if(sh_type < 12){
+            sh_type_str = SECTION_TYPES[sh_type];
+        } else if(sh_type >= SHT_LOPROC && sh_type <= SHT_HIPROC){
+            sh_type_str = "PROCESSOR-SPECIFIC";
+        } else if(shdr->sh_type >= SHT_LOUSER){
+            sh_type_str = "APPLICATION-SPECIFIC";
+        } else {
+            sh_type_str = "UNKNOWN";
+        } 
+        printf("\t\tsh_type = 0x%" PRIx32 " = %s\n", sh_type, sh_type_str);
+
+
+        const char *sh_flags_str;
+        uint8_t sh_flags = shdr->sh_flags & 0x7;
+        sh_flags_str = SECTION_FLAG_TYPES[sh_flags];
+        printf("\t\tsh_flags = 0x%" PRIu64 " = %s\n", shdr->sh_flags, sh_flags_str);
+
+
         printf("\t\tsh_addr = 0x%" PRIx64 "\n", shdr->sh_addr);
         printf("\t\tsh_offset = 0x%" PRIx64 "\n", shdr->sh_offset);
+        printf("\t\tsh_size = 0x%" PRIx64 "\n", shdr->sh_size);
+
+        // Special sh_link handling
+        printf("\t\tsh_link = 0x%" PRIx32 "\n", shdr->sh_link);
+        if(sh_type == SHT_DYNAMIC || sh_type == SHT_SYMTAB || sh_type == SHT_DYNSYM){
+            printf("\t\t\t--> section header table index to string table\n"); 
+        } else if(sh_type == SHT_HASH || sh_type == SHT_REL || sh_type == SHT_RELA){
+            printf("\t\t\t--> section header table index to symbol table\n");
+        }
+        
+
+        // Special sh_info handling
+        printf("\t\tsh_info = 0x%" PRIx32 "\n", shdr->sh_info);
+        if(sh_type == SHT_REL || sh_type == SHT_RELA){
+            printf("\t\t\t--> section header table index to relocatable section\n");
+        }
+        printf("\t\tsh_addralign = 0x%" PRIx64 "\n", shdr->sh_addralign);
+        printf("\t\tsh_entsize = 0x%" PRIx64 "\n", shdr->sh_entsize);
         shdr++;
     }
 
